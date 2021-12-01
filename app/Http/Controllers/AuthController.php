de74a9c0a71c510eb885d4307247d5f7107874f2
Jorge Eduardo <corpjorge@hotmail.com>
2021-12-01 18:40:36 -0500

    Login part 1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('home');
+        }
+
+        return back()->withErrors([
+            'email' => 'Las credenciales proporcionadas no coinciden con nuestros registros.',
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-//$router->get('/login', 'AsociadoController@entregar');
-$router->get('/', 'AsociadoController@index');
+$router->get('/', 'AuthController@index');
+$router->post('/login', 'AuthController@index');
+
+$router->get('/inicio', 'AsociadoController@index');
 $router->post('/buscar',  'AsociadoController@buscar');
 $router->get('/asociado/{asociado}', 'AsociadoController@asociado');
 $router->post('/entregar/{asociado}', 'AsociadoController@entregar');
