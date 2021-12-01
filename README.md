050ac470859c841677c5688ee8baa8f2c5e1ed82
Jorge Eduardo <corpjorge@hotmail.com>
2021-12-01 17:41:43 -0500

    Reset

--- README.md
+++
@@ -1,2 +0,0 @@
-
-## Cointramin Regalos Web
--- webpack.mix.js
+++
@@ -1,4 +0,0 @@
-const mix = require('laravel-mix');
-
-mix.sass('resources/scss/sb-admin-2.scss', 'public/css')
-    .js('resources/js/sb-admin-2.js', 'public/js')
